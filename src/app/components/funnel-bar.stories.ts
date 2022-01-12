e0871b45f41de4e72804125f00bbab935d807195
Griffin Kelton <griffin.kelton@terradotta.com>
2022-01-12 16:50:51 -0500

    commit on 'master'

--- src/app/components/funnel-bar.stories.ts
+++ src/app/components/funnel-bar.stories.ts
@@ -15,7 +15,7 @@ export default {
       },
       {
         name: 'hover',
-        link: '',
+        link: 'https://app.zeplin.io/project/61d5f289ed8d1e79ade68398/styleguide/components?coid=61df409115532232ebf49ddd',
       }
     ]
 }
